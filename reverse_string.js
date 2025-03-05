d0f4ccd0a1aae40d7da4a657b095f213ff4d632c
Yuvaraj <132157657+YuvarajRasu@users.noreply.github.com>
2025-03-05 08:29:58 +0530

    reverse string program

---
+++ reverse_string.js
@@ -0,0 +1,14 @@
+const original_string = "Hipnotizer";
+
+const reversedString = reverseString(original_string);
+console.log("The reversed string is :", reversedString);
+
+function reverseString(str) {
+  let reversedString = "";
+
+  for (let i = original_string.length - 1; i >= 0; i--) {
+    reversedString += str[i];
+  }
+
+  return reversedString;
+}
